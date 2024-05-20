5e0b67c4b80608dcacc70cc820cc2ab3b247d1d5
sgdxbc <sgdxbc@gmail.com>
2024-05-20 06:19:52 +0000

    Remove template components

---
+++ src/components/PostDetail.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { onMounted } from "vue";
+
+const props = defineProps({
+  path: {
+    type: String,
+    required: true,
+  }
+})
+
+import { ref } from 'vue'
+
+const detail = ref({
+  document: ["……"]
+})
+
+onMounted(() => {
+  console.log(`Mounted PostDetail for path ${props.path}`)
+  detail.value = {
+    title: "这是雪豹",
+    document: ["雪豹闭嘴。"]
+  }
+})
+</script>
+
+<template>
+  <h1 v-if="Object.hasOwn(detail, 'title')">{{ detail.title }}</h1>
+  <div v-for="item in detail.document" :key="item">
+    {{ item }}
+  </div>
+</template>
