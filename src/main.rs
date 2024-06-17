067a843bd6457a60dc789c4ad144c0dc27eae896
sgdxbc <sgdxbc@gmail.com>
2024-06-17 22:08:02 +0800

    Adjust style

--- src/main.rs
+++ src/main.rs
@@ -429,6 +429,8 @@ body {
     border-radius: 20px;
     padding: 1em 2em;
     position: relative;
+    font-family: Georgia, "Nimbus Roman No9 L", "Songti SC", "Noto Serif CJK SC", "Source Han Serif SC", "Source Han Serif CN", STSong, "AR PL New Sung", "AR PL SungtiL GB", NSimSun, SimSun, "TW\-Sung", "WenQuanYi Bitmap Song", "AR PL UMing CN", "AR PL UMing HK", "AR PL UMing TW", "AR PL UMing TW MBE", PMingLiU, MingLiU, serif;
+    font-size: 1.2em;
 }
 .note.current {
     outline: 1px dashed;
@@ -449,6 +451,17 @@ body {
 }
 .note hr {
     border-color: lightgray;
+}
+#footer {
+    margin-top: auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0 10px;
+    color: lightgray;
+    font-family: 
+        ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono", "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro","Fira Mono", "Droid Sans Mono", "Courier New", 
+        -apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp",
+        monospace;
 }
         "#;
         let footer = format!(
